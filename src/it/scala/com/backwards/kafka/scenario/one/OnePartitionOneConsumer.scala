495f1d96ee5a07292706ef6123d09dd58bda547c
David Ainslie <dainslie@gmail.com>
2019-08-06 21:11:08 +0100

    Forgot to use topicName

--- src/it/scala/com/backwards/kafka/scenario/one/OnePartitionOneConsumer.scala
+++ src/it/scala/com/backwards/kafka/scenario/one/OnePartitionOneConsumer.scala
@@ -16,7 +16,7 @@ object OnePartitionOneConsumer extends App with ScenarioFixture {
   val topicName = "one-partition-one-consumer"
   val groupName = s"$topicName-group"
 
-  val topic: NewTopic = createTopic("one-partition-one-consumer", numberOfPartitions = 1, replicationFactor = 1)
+  val topic: NewTopic = createTopic(topicName, numberOfPartitions = 1, replicationFactor = 1)
 
   val consume = doConsume[Foo](groupName, topic.name)
   val produce = doProduce(Foo("some-thing"), topic.name)
